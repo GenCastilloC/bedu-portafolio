4a8486088613b6d0ccdec4039f6fb58a333db56b
GenCastilloC <gencastilloc@gmail.com>
2020-06-13 21:02:04 -0500

    se agregan botones y cantidad

--- Sesion05/app.js
+++ Sesion05/app.js
@@ -4,20 +4,78 @@
  * 3.- Sumar los valores
  * 4.- Imprimir el resultado en el tag
  */
-function calculator() {
-    var btn = document.getElementById('calculate');
+function sumar() {
+    var btn = document.getElementById('add');
 
     btn.addEventListener('click', function(e) {
         e.preventDefault();
 
         var input1 = document.getElementById("number-one");
         var input2 = document.getElementById("number-two");
+        var input3 = document.getElementById("number-tree");
 
-        var sumResult = +input1.value + +input2.value;
+        var sumResult = +input1.value + +input2.value + +input3.value;
 
         var result = document.getElementById("result");
 
         result.textContent = sumResult;
     });
 }
-calculator();
+sumar();
+
+function restar() {
+    var btn = document.getElementById('subtraction');
+
+    btn.addEventListener('click', function(e) {
+        e.preventDefault();
+
+        var input1 = document.getElementById("number-one");
+        var input2 = document.getElementById("number-two");
+        var input3 = document.getElementById("number-tree");
+
+        var sumResult = +input1.value - +input2.value - +input3.value;
+
+        var result = document.getElementById("result");
+
+        result.textContent = sumResult;
+    });
+}
+restar();
+
+function multiplicar() {
+    var btn = document.getElementById('multiplication');
+
+    btn.addEventListener('click', function(e) {
+        e.preventDefault();
+
+        var input1 = document.getElementById("number-one");
+        var input2 = document.getElementById("number-two");
+        var input3 = document.getElementById("number-tree");
+
+        var sumResult = +input1.value * +input2.value * +input3.value;
+
+        var result = document.getElementById("result");
+
+        result.textContent = sumResult;
+    });
+}
+multiplicar();
+
+function dividir() {
+    var btn = document.getElementById('division');
+
+    btn.addEventListener('click', function(e) {
+        e.preventDefault();
+
+        var input1 = document.getElementById("number-one");
+        var input2 = document.getElementById("number-two");
+        var input3 = document.getElementById("number-tree");
+
+        var sumResult = +input1.value / +input2.value / +input3.value;
+
+        var result = document.getElementById("result");
+
+        result.textContent = sumResult;
+    });
+}
+dividir();
